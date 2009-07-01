dfdf90854a7aab19af5c52af544ad811fe7b10c7
Joseph Turian <turian@gmail.com>
2009-07-01 01:08:00 -0400

    Cleaned up graph.py a bit.

--- graph.py
+++ graph.py
@@ -15,7 +15,7 @@ MTYPE = SS.csr_matrix
 TMTYPE = TS.csr_matrix
 #_MTYPEs = [sparse.csc_matrix, sparse.csr_matrix, sparse.dok_matrix, sparse.lil_matrix, sparse.coo_matrix]
 # * new class ``dia_matrix`` : the sparse DIAgonal format
-# * new class ``bsr_matrix`` : the Block CSR format
+# * new class ``bsr_matrix`` : the Block CS format
 
 import theano.tensor.nnet as nnet
 #ACTIVATION_FUNCTION = nnet.sigmoid
@@ -24,30 +24,26 @@ import theano.tensor.nnet as nnet
 from theano.sandbox.softsign import softsign
 ACTIVATION_FUNCTION = softsign
 
-xR = TMTYPE('x')
+x = TMTYPE()
 #print targ1.shape
-targetyR = TT.lvector()
-#print xR, targR
+targety = TT.lvector()
+#print x, targ
 
 #random_weights
-w1R = TT.dmatrix('w1')
-b1R = TT.dvector('b1')
-w2R = TT.dmatrix('w2')
-b2R = TT.dvector('b2')
+w1 = TT.dmatrix()
+b1 = TT.dvector()
+w2 = TT.dmatrix()
+b2 = TT.dvector()
 
 from theano.tensor.nnet import crossentropy_softmax_argmax_1hot_with_bias
 from theano.compile.function_module import function
 
-#xw1R = theano.dot(w1R.T, xR.T).T
-xw1R = TS.structured_dot(w1R.T, xR.T).T
-#print w1R.type
-#print xR.type
-
-hR = ACTIVATION_FUNCTION(xw1R + b1R)
+xw1 = TS.structured_dot(w1.T, x.T).T
+h = ACTIVATION_FUNCTION(xw1 + b1)
 
 #zero = tensor.zeros_like(x[0,:])
-(kl, softmax, argmax) = crossentropy_softmax_argmax_1hot_with_bias(theano.dot(hR, w2R), b2R, targetyR)
+(kl, softmax, argmax) = crossentropy_softmax_argmax_1hot_with_bias(theano.dot(h, w2), b2, targety)
 
-validatefn = function([xR, targetyR, w1R, b1R, w2R, b2R], [kl, softmax, argmax, xw1R], mode=COMPILE_MODE)
-(gw1, gb1, gw2, gb2) = TT.grad(kl, [w1R, b1R, w2R, b2R])
-trainfn = function([xR, targetyR, w1R, b1R, w2R, b2R], [kl, softmax, argmax, xw1R, theano.compile.io.Out(gw1, borrow = True), gb1, gw2, gb2], mode=COMPILE_MODE)
+validatefn = function([x, targety, w1, b1, w2, b2], [kl, softmax, argmax, xw1], mode=COMPILE_MODE)
+(gw1, gb1, gw2, gb2) = TT.grad(kl, [w1, b1, w2, b2])
+trainfn = function([x, targety, w1, b1, w2, b2], [kl, softmax, argmax, xw1, theano.compile.io.Out(gw1, borrow = True), gb1, gw2, gb2], mode=COMPILE_MODE)
